bee9b983b81bf803ae7b36cbc3acdabfe1c3f601
zane108231 <johnlongno4@gmail.com>
2024-11-26 18:22:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.get("/", (_, res) => res.sendFile(__dirname + "/index.html"));
 app.listen(process.env.PORT);
 
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.repl.co/`);
-}, 224000);
+  bot.swingArm();
+}, 5000);  // Swing the arm every 5 seconds
+
 
 
 // U CAN ONLY EDIT THIS SECTION!!
